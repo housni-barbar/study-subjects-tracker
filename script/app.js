005527e7bad66e4922dba2a1e9bafd679f186585
housni-barbar <housnibarbar16@icloud.com>
2026-02-22 13:56:53 +0300

    fix service worker

--- script/app.js
+++ script/app.js
@@ -466,6 +466,10 @@ function render() {
 
 render();
 if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./sw.js")
+    .then(() => console.log("Service Worker Registered"))
+    .catch(err => console.log("SW Error:", err));
+
   navigator.serviceWorker.addEventListener("controllerchange", () => {
     window.location.reload();
   });
